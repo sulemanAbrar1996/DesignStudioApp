3194d1836e1690a954e837bd957d65cd9eaa184b
Suleman Abrar <rana.suleman.abrar1@gmail.com>
2024-11-17 15:23:58 +0500

    Initial Commit

---
+++ DesignStudioApp/DesignStudioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignStudioAppApp.swift
+//  DesignStudioApp
+//
+//  Created by Suleman Abrar on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct DesignStudioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
